aa7511c891097d0411c6608bf6828a9d9d722258
ludbakazar <ludbakazarr@gmail.com>
2025-08-15 10:27:21 +0700

    Implement GET, DELETE, and PUT endpoints for managing suppliers; enhance error handling

--- src/app/api/finances/suppliers/route.js
+++ src/app/api/finances/suppliers/route.js
@@ -28,3 +28,58 @@ export async function POST(request) {
     return errorHandler(error);
   }
 }
+
+export async function GET(request) {
+  try {
+    const suppliers = await prisma.suppliers.findMany({
+      where: { status: true },
+    });
+    return Response.json(suppliers);
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
+
+export async function DELETE(request) {
+  try {
+    const userId = Number(request.headers.get("userId"));
+    const { supplierId } = await request.json();
+    await prisma.suppliers.update({
+      where: { id: supplierId },
+      data: {
+        status: false,
+        updatedByUserId: userId,
+        updatedAt: new Date(),
+      },
+    });
+    return Response.json("Supplier deleted successfully");
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
+
+export async function PUT(request) {
+  try {
+    const userId = Number(request.headers.get("userId"));
+    const { supplierId, code, name, phone, address } = await request.json();
+
+    await prisma.suppliers.update({
+      where: { id: supplierId },
+      data: {
+        code: code.toUpperCase(),
+        name: name.toUpperCase(),
+        phone: phone.toUpperCase(),
+        address: address.toUpperCase(),
+        updatedByUserId: userId,
+        updatedAt: new Date(),
+      },
+    });
+
+    return Response.json(
+      { message: "Supplier updated successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
